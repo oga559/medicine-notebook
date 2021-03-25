bbbb58ed36c485ea18173c5f4d1772687aac905f
oga559 <coolsky.4649@gmail.com>
2021-03-25 21:34:50 +0900

    マイグレーション完成

---
+++ database/migrations/2021_03_25_192944_crate_photo_post_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CratePhotoPostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photo_posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('medical_factories_id');
+            $table->binary('photo');
+            $table->datetime('prescription_date');
+            $table->string('medical_subjects', 100);
+            $table->text('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photo_posts');
+    }
+}
